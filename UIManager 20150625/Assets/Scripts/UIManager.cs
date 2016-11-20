21692efc6196ba32f7e3e65684cae51fd90f1e86
Zhang Yuchen <zhangqnick@foxmail.com>
2016-11-20 18:16:21 +0800

    UGUI框架和优化系列

---
+++ UIManager 20150625/Assets/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject startScreen;
+	public string outTrigger;
+	private List<GameObject> screenHistory;
+
+	void Awake(){
+
+        //this.screenHistory = new List<GameObject>{this.startScreen};
+        this.screenHistory = new List<GameObject>();
+        screenHistory.Add(this.startScreen);
+	}
+
+	public void ToScreen(GameObject target){
+
+		GameObject current = this.screenHistory[this.screenHistory.Count - 1];
+
+		if(target == null || target == current) return;
+
+		this.PlayScreen(current , target , false , this.screenHistory.Count);
+		this.screenHistory.Add(target);
+	}
+
+	public void GoBack(){
+
+		if(this.screenHistory.Count > 1){
+
+			int currentIndex = this.screenHistory.Count - 1;
+			this.PlayScreen(this.screenHistory[currentIndex] , this.screenHistory[currentIndex - 1] , true , currentIndex - 2);
+			this.screenHistory.RemoveAt(currentIndex);
+		}
+	}
+
+	private void PlayScreen(GameObject current , GameObject target , bool isBack , int order){
+
+		current.GetComponent<Animator>().SetTrigger(this.outTrigger);
+
+		if(isBack){
+
+			current.GetComponent<Canvas>().sortingOrder = order;
+
+		}else{
+
+			current.GetComponent<Canvas>().sortingOrder = order - 1;
+			target.GetComponent<Canvas>().sortingOrder = order;
+		}
+
+		target.SetActive(true);
+	}
+}
---
+++ UIManager 20150625/Assets/Scripts/UIStateClosed.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class UIStateClosed : StateMachineBehaviour {
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	
+		animator.gameObject.SetActive(false);
+	}
+}
